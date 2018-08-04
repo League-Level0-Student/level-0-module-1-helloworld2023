c60418ecbe0fc414b28570691a85f590b66b572e
helloworld2023 <league@WTS14.attlocal.net>
2018-08-04 11:28:50 -0700

    I started Level 0 Module 1

---
+++ src/extra/Unbirthday.java
@@ -0,0 +1,18 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+
+		String birthday = JOptionPane.showInputDialog("When is your birthday? (mm/dd)");
+		if (birthday.equals("8/4")) {
+			JOptionPane.showMessageDialog(null, "Happy Birthday!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Merry Unbirthday!");
+		}
+
+	}
+
+}
