9dfeaac7ab0235ea3ad46b3167e5ef4b8a680784
league <league@wts-imac-11.attlocal.net>
2018-08-20 16:56:01 -0700

    MyAges, Badgers

---
+++ src/ifs_ints_and_loops/Badgers.java
@@ -0,0 +1,26 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Badgers {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 2; i++) {
+
+			for (int i1 = 1; i1 < 11; i1++) {
+				System.out.print("badger, ");
+			}
+			System.out.println();
+			for (int i2 = 1; i2 < 3; i2++) {
+				System.out.print("mushroom, ");
+			}
+			System.out.println("a");
+
+			//*Ms. Jackie said that I just had to do the first 2 verses.
+			
+		}
+
+	}
+
+}
---
+++ src/ifs_ints_and_loops/MyAges.java
@@ -0,0 +1,14 @@
+package ifs_ints_and_loops;
+
+public class MyAges {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i < 14; i++) {
+			System.out.println(i);
+		}		
+		
+	}
+	
+	
+}
